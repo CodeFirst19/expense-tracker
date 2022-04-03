2c743decd0483fc36bfd2510ac08e704e78cfa54
Kamogelo Mokgwatjane <kamogelomokgwatjane@sparksmiths.com>
2022-04-03 23:30:07 +0200

    Responsiveness

--- lib/widgets/new_transaction.dart
+++ lib/widgets/new_transaction.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class NewTransaction extends StatefulWidget {
 
-  final void Function(String, double) addHandler;
+  final void Function(String, double, DateTime) addHandler;
 
   NewTransaction({required this.addHandler});
 
@@ -11,53 +12,99 @@ class NewTransaction extends StatefulWidget {
 }
 
 class _NewTransactionState extends State<NewTransaction> {
-  final titleController = TextEditingController();
+  final _titleController = TextEditingController();
+  final _amountController = TextEditingController();
+  DateTime? _selectedDate;
 
-  final amountController = TextEditingController();
+  void _submitData() {
+    final title = _titleController.text;
+    final amount = double.parse(_amountController.text);
 
-  void submitData() {
-    final title = titleController.text;
-    final amount = double.parse(amountController.text);
-
-    if(title.isEmpty || amount <= 0) {
+    if(title.isEmpty || amount <= 0 || _selectedDate == null) {
       return;
     }
 
     // widget helps to access the properties of the widget class (NewTransaction), even in my state class (current)
     // because after all, all this is just a single widget
-    widget.addHandler(title, amount);
+    widget.addHandler(title, amount, _selectedDate!);
 
     // Close modal upon clicking add
     //This context arg here gives access to context related to my widget
     Navigator.of(context).pop();
   }
 
+  void _presentDatePicker() {
+    showDatePicker(
+        context: context,
+        initialDate: DateTime.now(),
+        firstDate: DateTime(2022),
+        lastDate: DateTime.now()
+    ).then((pickedDate) {
+      if (pickedDate == null) {
+        return;
+      }
+
+      setState(() {
+        _selectedDate = pickedDate;
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Card(elevation: 5, child: Container(
-      padding: const EdgeInsets.all(10),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.end,
-        children: [
-          TextField(
-            decoration: const InputDecoration(labelText: 'Title'),
-            controller: titleController,
-            onSubmitted: (_) => submitData(),
-          ),
-          TextField(
-            decoration: const InputDecoration(labelText: 'Amount'),
-            controller: amountController,
-            keyboardType: TextInputType.number,
-            onSubmitted: (_) => submitData(),
-          ),
-          TextButton(
-              child: const Text('Add Transaction'),
-              style: TextButton.styleFrom(primary: Colors.purple),
-              onPressed: submitData,
-          )
-        ],
+    return SingleChildScrollView(
+      child: Card(elevation: 5, child: Container(
+        padding: EdgeInsets.only(
+            top: 10,
+            left: 10,
+            right: 10,
+            bottom: MediaQuery.of(context).viewInsets.bottom + 10
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            TextField(
+              decoration: const InputDecoration(labelText: 'Title'),
+              controller: _titleController,
+              onSubmitted: (_) => _submitData(),
+            ),
+            TextField(
+              decoration: const InputDecoration(labelText: 'Amount'),
+              controller: _amountController,
+              keyboardType: TextInputType.number,
+              onSubmitted: (_) => _submitData(),
+            ),
+            Container(
+              height: 80,
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Text(
+                        _selectedDate == null
+                            ? 'No Date Chosen'
+                            : 'Chosen Date: ${DateFormat.yMd().format(_selectedDate!)}'
+                    ),
+                  ),
+                  TextButton(
+                      onPressed: _presentDatePicker,
+                      child: const Text('Choose Date', style: TextStyle(fontWeight: FontWeight.bold),),
+                      style: TextButton.styleFrom(primary: Theme.of(context).primaryColor),
+                  ),
+                ],
+              ),
+            ),
+            ElevatedButton(
+                onPressed: _submitData,
+                child: const Text('Add Transaction'),
+                style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(Theme.of(context).primaryColor),
+                    foregroundColor: MaterialStateProperty.all(Theme.of(context).textTheme.button?.color)
+                ),
+            ),
+          ],
+        ),
       ),
-    ),
+      ),
     );
   }
 }
